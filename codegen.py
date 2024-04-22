afbd250e2774160fdbff17e9c07626c4f3c00a8c
yu-hsieh <Yu-cheng.Hsieh@amd.com>
2024-04-22 02:45:07 +0000

    change name to dev line

--- codegen.py
+++ codegen.py
@@ -147,10 +147,9 @@ class AMaxKernelGenerator:
 
     @property
     def func_name(self):
-        if is_scale:
-            return 'AMax_Scale'
-        else:
-            return 'AMax'
+        if self.is_scale:
+            return f'AMax_Ti_{self.i_type}_To_{self.o_type}_Ts_{self.scale_type}'
+        return f'AMax_Ti_{self.i_type}_To_{self.o_type}'
 
     def dumps(self, format: str) -> str:
         param_dict = {
